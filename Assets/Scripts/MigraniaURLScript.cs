a8e8fccc7afb8d050f00f49ce66d1a8a9a7bc8f8
eli094 <elizabethiscool094@gmail.com>
2026-04-11 19:41:46 -0300

    ManyChnagesAndThingsDone
    Agregado:
    
    - Nuevos textos (Character, Link y Logo).
    - Animaciones para el título y la estrella.
    
    Corregidos:
    
    - Los botones.
    
    Co-Authored-By: ArchMagosF04 <166256667+ArchMagosF04@users.noreply.github.com>

--- Assets/Scripts/MigraniaURLScript.cs
+++ Assets/Scripts/MigraniaURLScript.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class MigraniaURLScript : MonoBehaviour
 {
+    [SerializeField] private Animator logoStarAnimation;
+
     public void GoToURL(string url)
     {
         Application.OpenURL(url);
+    }
+
+    public void StarLogoAnimation()
+    {
+        logoStarAnimation.SetTrigger("Spin");
     }
 }
